38f34dec85cfbae3eedf85d60ea6610d5ccbd68f
Eliam2102 <157552853+Eliam2102@users.noreply.github.com>
2024-10-31 23:15:17 -0600

    fix-navbar styles
    en Este commit se agregan arreglos de estilos del navbar para un aspecto más limpio y comenzando a implementar un patrón de diseño

--- src/components/NavBar/components/NavBar.tsx
+++ src/components/NavBar/components/NavBar.tsx
@@ -7,30 +7,28 @@ import "../assets/styles/NavBar.css";
 const Navbar: React.FC = () => {
   const location = useLocation();
 
-  // Estado para controlar la apertura/cierre del menú móvil
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const isActive = (path: string): string => {
     return location.pathname === path ? 'active' : '';
   };
 
-  // Función para alternar el menú móvil
+  const toggleDropdown = (): void => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  const closeDropdown = (): void => {
+    setDropdownOpen(false);
+  };
+
   const toggleMobileMenu = (): void => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
     <nav className="navbar">
-      {/* Primer Div */}
-      <div className="navbar-top">
-        <div className="contact-info"></div>
-        <div className="auth-links">
-          <Link to="/login" className="nav-link">Iniciar Sesión</Link>
-          <Link to="/account" className="nav-link">Mi Cuenta</Link>
-        </div>
-      </div>
-
-      {/* Segundo Div */}
+      {/* Logo */}
       <div className="navbar-middle">
         <Link to="/" className="navbar-brand">
           <img
@@ -41,26 +39,43 @@ const Navbar: React.FC = () => {
             alt="Brand Logo"
           />
         </Link>
-        <div className="navbar-search-cart">
-        <ul className="navbar-nav-web">
-          <li className="nav-item">
-            <Link to="/" className={`nav-link ${isActive("/")}`}>Inicio</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about-us" className={`nav-link ${isActive("/about-us")}`}>Nosotros</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/products" className={`nav-link ${isActive("/products")}`}>Productos</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contactus" className={`nav-link ${isActive("/contactus")}`}>Contáctenos</Link>
-          </li>
-        </ul>
-          <div className="btn-cart">
-            <Link to="/Cart">
-              <img src={iconCart} width="25" height="25" alt="Cart Icon" />
-            </Link>
+
+        {/* Dropdown y carrito */}
+        <div className="navbar-right">
+          <div className="navbar-dropdown">
+          <button onClick={toggleDropdown} className="dropdown-toggle">
+            <div className={`hamburger-icon ${isDropdownOpen ? 'open' : ''}`}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </button>
+            <ul className={`dropdown-menu ${isDropdownOpen ? 'open' : ''}`}>
+              <li className="nav-item">
+                <Link to="/about-us" className={`nav-link ${isActive("/about-us")}`} onClick={closeDropdown}>Nosotros</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/products" className={`nav-link ${isActive("/products")}`} onClick={closeDropdown}>Productos</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contactus" className={`nav-link ${isActive("/contactus")}`} onClick={closeDropdown}>Contáctenos</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/login" className={`nav-link ${isActive("/login")}`} onClick={closeDropdown}>Iniciar sesión</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/account" className={`nav-link ${isActive("/account")}`} onClick={closeDropdown}>Mi cuenta</Link>
+              </li>
+            </ul>
           </div>
+
+          {/* Carrito */}
+          {/* Carrito */}
+            <div className={`btn-cart ${isActive("/Cart") ? 'active' : ''}`}>
+              <Link to="/Cart">
+                <img src={iconCart} width="25" height="25" alt="Cart Icon" />
+              </Link>
+            </div>
         </div>
       </div>
 
@@ -73,9 +88,6 @@ const Navbar: React.FC = () => {
       <div className={`navbar-mobile-menu ${isMobileMenuOpen ? 'active' : ''}`}>
         <div className="close-icon" onClick={toggleMobileMenu}></div>
         <ul className="navbar-nav">
-          <li className={`nav-item ${isActive("/")}`}>
-            <Link to="/" className="nav-link" onClick={toggleMobileMenu}>Inicio</Link>
-          </li>
           <li className={`nav-item ${isActive("/about-us")}`}>
             <Link to="/about-us" className="nav-link" onClick={toggleMobileMenu}>Nosotros</Link>
           </li>
@@ -84,6 +96,12 @@ const Navbar: React.FC = () => {
           </li>
           <li className={`nav-item ${isActive("/contactus")}`}>
             <Link to="/contactus" className="nav-link" onClick={toggleMobileMenu}>Contáctenos</Link>
+          </li>
+          <li className={`nav-item ${isActive("/login")}`}>
+            <Link to="/login" className="nav-link" onClick={toggleMobileMenu}>Iniciar sesión</Link>
+          </li>
+          <li className={`nav-item ${isActive("/account")}`}>
+            <Link to="/account" className="nav-link" onClick={toggleMobileMenu}>Mi cuenta</Link>
           </li>
         </ul>
       </div>
