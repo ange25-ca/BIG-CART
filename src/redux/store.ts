f292835366742922624a16c5ce05799765a1f253
Ange <monsecahuum@gmail.com>
2024-11-15 00:05:04 -0600

    Se agregan el manejo del ID del usuario

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  productosReducer  from "./productosSlice";
+import userReducer from "./userSlices";
 
 const store = configureStore({
     reducer: {
         productos: productosReducer,
+        user: userReducer,
     }
 });
 
