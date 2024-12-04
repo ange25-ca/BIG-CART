5273de60f561424260631f7fcd56c216218810e5
antoniouc <antoniouc38@gmail.com>
2024-12-04 13:21:16 -0600

    se agrega otra ves el plugin xd

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import  store  from './redux/store.ts'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
         <App/>
-      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 )
