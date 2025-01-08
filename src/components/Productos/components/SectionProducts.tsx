c60fd900e4ce32becf400788dc366f5ae135d1fc
Eliam2102 <157552853+Eliam2102@users.noreply.github.com>
2025-01-08 15:33:54 -0600

    fix de filtros
    final se agrego mas funcionalidad para que abarque todos los productos

--- src/components/Productos/components/SectionProducts.tsx
+++ src/components/Productos/components/SectionProducts.tsx
@@ -24,6 +24,8 @@ const priceRanges = [
   { id: 1, label: '0-50', min: 0, max: 50 },
   { id: 2, label: '50-100', min: 50, max: 100 },
   { id: 3, label: '100-200', min: 100, max: 200 },
+  { id: 4, label: '300-400', min: 300, max: 400 },
+  { id: 5, label: '500+', min: 500, max: 100000 }
 ];
 
 const CatalogsProducts: React.FC = () => {
